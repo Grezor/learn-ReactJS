6ffe56895facb40968d616e1868bfb95be4a921b
Geoffrey Duplessi <grezor.gamer@gmail.com>
2020-11-06 14:58:44 +0100

    event, states

--- src/App.js
+++ src/App.js
@@ -4,13 +4,45 @@ import React, { Component } from 'react';
 import Mycars from './components/Mycars';
 
 class App extends Component {
+  
   state = {
-    titre: 'Mon Catalogue voitures'
+    titre: 'Mon Catalogue voitures',
+    para: 'je suis un para'
   }
+
+  changeTitle = (e) => {
+    console.log(e.target);
+    this.setState({
+      titre: 'mon nouveau titre'
+    })
+  }
+  changeViaParam = (titre) => {
+    this.setState({
+      titre: titre
+    })
+  }
+
+  changeViabind = (param) => {
+    this.setState({
+      titre: param
+    })
+  }
+
+  changeViaInput = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      para: e.target.value
+    })
+  }
+
   render(){
     return (
       <div className="App">
-        <Mycars title={this.state.titre} />
+        <Mycars title={this.state.titre} paragraphe={this.state.para}/>
+        <button onClick={this.changeTitle}>Changer le nom en dur</button>
+        <button onClick={()=> this.changeViaParam('titre via un param')}>Changer le param</button>
+        <button onClick={this.changeViabind.bind(this, 'titre via bind')}>via bind</button>
+        <input type="text" onChange={this.changeViaInput} value={this.state.para} />
       </div>
     );
   }
--- src/components/Mycars.js
+++ src/components/Mycars.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react'
 import Cars from './Cars';
 
 class Mycars extends Component {
+    noCopy = () => {
+        alert('merci de ne pas copier le texte');
+    }
+
+    addStyle = (e) => {
+        console.log(e.target);
+        if (e.target.classList.contains('styled')) {
+            e.target.classList.remove('styled');
+        } else {
+            e.target.classList.toggle('styled');
+        }
+    }
+
     render(){
         console.log(this);
         return(
             <div>
                 <h1>{this.props.title}</h1>
+                <p onCopy={this.noCopy}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi fugit eveniet
+                 facere aperiam iusto
+                ut consectetur, praesentium molestias, natus, nihil aliquid adipisci delectus odio optio dolor?
+                Nemo officia corrupti quasi!
+                </p>
+                <p onMouseOver={this.addStyle} className="styled">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quas veniam, sequi expedita quis alias, quam nobis iste quae odit maiores illum? A doloremque accusantium quidem sit natus qui voluptatum.</p>
+                <p>{this.props.paragraphe}</p>
                 <Cars color="red">Renault</Cars>
                 <Cars >Peugeot</Cars>
                 <Cars color="yello"></Cars>
